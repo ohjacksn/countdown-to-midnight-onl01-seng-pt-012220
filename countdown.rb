b51d47fbf5d74578b7774f4c8f24c2f868c37273
Jackson Mo <jacksonmo@me.com>
2020-01-25 04:51:41 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(integer)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+  while integer > 0 do
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
